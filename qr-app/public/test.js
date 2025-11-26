95d1117e3cae2dd20a879d55e7c6b93cda17cf52
2i-7 <niina.umekita@gmail.com>
2025-11-26 14:57:09 +0900

    送信ボタンとコンソール表示のロジック作成

---
+++ qr-app/public/test.js
@@ -0,0 +1,30 @@
+function profileData(){
+    let nameInput = document.getElementById("name")
+    let nameValue = nameInput.value;
+
+    let allBoxes = document.querySelectorAll('.tag-input');
+    let bitString = "";
+
+    if (nameValue == "") {
+        alert("名前が入力されていません");
+        return;
+    }
+
+    allBoxes.forEach(function(box) {
+        if (box.checked == true) {
+            // チェックされてたら "1" 
+            bitString = bitString + "1"; 
+        } else {
+            // されてなかったら "0" 
+            bitString = bitString + "0"; 
+        }
+    });
+
+    let profileData = {
+        "name": nameValue,
+        "likes": bitString
+    };
+
+    console.log("--- 送信データ ---");
+    console.log(profileData);
+}
