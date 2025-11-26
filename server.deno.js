f4b4cf0fe51f60cc5b48f92aceecd612ddd3f628
nari88118 <109565861+nari88118@users.noreply.github.com>
2025-11-26 14:59:38 +0900

    テストDB実装

--- server.deno.js
+++ server.deno.js
@@ -5,7 +5,15 @@ Deno.serve(async (req) => {
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+    const kv = await Deno.openKv();  // DBを開く（または新規作成）
+
+    // 値を保存（または更新）
+    await kv.set(["user", "alice"], { name: "Alice", age: 30 });
+
+    // 値を取得
+    const entry = await kv.get(["user", "alice"]);
+
+    return new Response(JSON.stringify(entry.value));
   }
 
   return serveDir(req, {
