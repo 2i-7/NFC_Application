a7b0b5daecc57e213b926f4a2d8f601070d62147
KazuyaMshio <gorogundam@gmail.com>
2025-11-27 11:30:26 +0900

    css

--- public/QRcode.js
+++ public/QRcode.js
@@ -3,4 +3,4 @@ let params = new URLSearchParams(document.location.search);
 var genelate_url = `${window.location.origin}/display.html?id=${params.get("id")}`;
 console.log(genelate_url);
 
-document.getElementById('url').src = "https://api.qrserver.com/v1/create-qr-code/?data="+genelate_url+"&size=400x400";
+document.getElementById('url').src = "https://api.qrserver.com/v1/create-qr-code/?data="+genelate_url+"&size=500x500";
